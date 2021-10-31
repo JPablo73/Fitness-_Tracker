b01cbc2c19f148266c806fe1c1b9d15ca4df340f
JPablo73 <82916926+JPablo73@users.noreply.github.com>
2021-10-31 10:33:13 -0700

    edit server.js

--- server.js
+++ server.js
@@ -2,31 +2,31 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000;
-
 const app = express();
+
+const PORT = process.env.PORT || 3000;
 
 app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
-
-// Mongoose db connection
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/fitnesstracker",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  }
-);
 
 // routes
 app.use(require("./routes/api-routes.js"));
 app.use(require("./routes/html-routes.js"));
+
+// Mongoose db connection
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/fitnesstracker", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+
+  .then(() => console.log("Connected to Mongodb"))
+  .catch((err) => console.log(err));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
